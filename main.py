e2fcf24c07a198accd054d8fcf0db3bb21488813
Alec White <5921175+alec0xFFFFFF@users.noreply.github.com>
2023-11-23 09:25:22 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from api.api import create_api
-
+from api.routes import init_api
 api = create_api()
-routes.init_app(api)
+init_api(api)
 
 if __name__ == '__main__':\
     api.run(debug=True, port=os.getos.environ.get("PORT", default=5000))
