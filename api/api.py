48e2324fb56a36832506fba50cfe98ffc7bddbb8
Alec White <5921175+alec0xFFFFFF@users.noreply.github.com>
2023-11-23 09:36:53 -0800

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,6 +1,12 @@
 from flask import Flask
 
 def create_api():
-    api = Flask(__name__)
-    return api
+    app = Flask(__name__)
+
+    # Configuration and other setup here
+
+    from api.routes import init_app
+    init_app(app)
+
+    return app
 
