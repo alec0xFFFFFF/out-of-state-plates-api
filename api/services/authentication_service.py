281a51af89ca1d063b801a1351f7439dcc322009
Alec White <5921175+alec0xFFFFFF@users.noreply.github.com>
2023-11-24 08:57:17 -0800

    Create authentication_service.py

---
+++ api/services/authentication_service.py
@@ -0,0 +1,20 @@
+class InvalidCredentialsError(Exception):
+    """Exception raised for invalid credentials."""
+
+    def __init__(self, message="Invalid username or password"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class AuthenticationService:
+  def __init__(self, db):
+    self.db = db
+
+  def login(self, request):
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    user = User.query.filter_by(username=username).first()
+
+    if user and check_password_hash(user.password_hash, password):
+        return create_access_token(identity=username)
+    raise InvalidCredentialsError()
