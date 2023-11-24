5ef945b822f9b356afa6adb39b8b0f813c910fe6
Alec White <5921175+alec0xFFFFFF@users.noreply.github.com>
2023-11-24 07:47:42 -0800

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,5 +1,14 @@
 from flask import Flask
 from data.models import db
+import os
+
+db_params = {
+    "dbname": os.environ.get("PGDATABASE"),
+    "user": os.environ.get("PGUSER"),
+    "password": os.environ.get("PGPASSWORD"),
+    "host": os.environ.get("PGHOST"),
+    "port": int(os.environ.get("PGPORT"))
+}
 
 def create_api():
     app = Flask(__name__)
@@ -7,7 +16,7 @@ def create_api():
     # Authentication configuration
     app.config['JWT_SECRET_KEY'] = os.environ.get("JWT_SECRET_KEY")
     # Database configuration
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/dbname'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_params.user}:{db_params.password}@{db_params.host}:{db_params.port}/{db_params.dbname}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)    
