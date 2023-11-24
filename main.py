70b71098deb9e6f9b4a86af9c4d89beeb44acd1a
Alec White <5921175+alec0xFFFFFF@users.noreply.github.com>
2023-11-24 07:37:15 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from api.api import create_api
+from data.models import db
 
 app = create_api()
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':\
     app.run(debug=True, port=os.getos.environ.get("PORT", default=5000))
