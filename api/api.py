5d77a58445ff8d810e8d66877e166672deafdee9
Alec White <5921175+alec0xFFFFFF@users.noreply.github.com>
2023-11-24 07:35:21 -0800

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from api.models import db
 
 def create_api():
     app = Flask(__name__)
 
-    # Configuration and other setup here
+    # Authentication configuration
+    app.config['JWT_SECRET_KEY'] = os.environ.get("JWT_SECRET_KEY")
+    # Database configuration
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/dbname'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)    
+    jwt = JWTManager(app)
 
     from api.routes import init_app
     init_app(app)
