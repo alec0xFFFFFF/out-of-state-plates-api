c28130a09c3bdac55042767a21cdbc4c70d18ff6
Alec White <5921175+alec0xFFFFFF@users.noreply.github.com>
2023-11-26 20:50:43 -0800

    Update models.py

--- data/models.py
+++ data/models.py
@@ -11,7 +11,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     name = db.Column(db.String(120), nullable=False)
     phone = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.Text)
     created_at = db.Column(DateTime, default=datetime.utcnow)
     updated_at = db.Column(DateTime, onupdate=datetime.utcnow)
   
