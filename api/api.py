23878d3560e07aae63480b5ec0717c78c16ba85a
Alec White <5921175+alec0xFFFFFF@users.noreply.github.com>
2023-11-26 18:35:52 -0800

    Update api.py

--- api/api.py
+++ api/api.py
@@ -3,21 +3,13 @@ from flask_jwt_extended import JWTManager
 from data.models import db
 import os
 
-db_params = {
-    "dbname": os.environ.get("PGDATABASE"),
-    "user": os.environ.get("PGUSER"),
-    "password": os.environ.get("PGPASSWORD"),
-    "host": os.environ.get("PGHOST"),
-    "port": int(os.environ.get("PGPORT"))
-}
-
 def create_api():
     app = Flask(__name__)
 
     # Authentication configuration
     app.config['JWT_SECRET_KEY'] = os.environ.get("JWT_SECRET_KEY")
     # Database configuration
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_params["user"]}:{db_params["password"]}@{db_params["host"]}:{db_params["port"]}/{db_params["dbname"]}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("PG_DB_URI")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)    
